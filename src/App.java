1960b8e6bbc7c939715f242612597f1435bf9e14
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-02 20:23:40 +0200

    complete day 2 part 1

--- src/App.java
+++ src/App.java
@@ -1,12 +1,16 @@
 // import day1.Day1;
-import day1.Day1Part2;
+// import day1.Day1Part2;
+import day2.Day2;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // int result = new Day1().processFile();
         // System.out.println(result);
 
-        int result = new Day1Part2().processFile();
+        // int result = new Day1Part2().processFile();
+        // System.out.println(result);
+
+        int result = new Day2().processFile();
         System.out.println(result);
     }
 }
