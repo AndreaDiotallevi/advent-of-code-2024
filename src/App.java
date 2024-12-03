7278b31ffa77dea37d1afca553d74ca9e7d53650
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-03 19:45:05 +0200

    feat: complete day 3 part 2

--- src/App.java
+++ src/App.java
@@ -2,7 +2,8 @@
 // import day1.Day1Part2;
 // import day2.Day2;
 // import day2.Day2Part2;
-import day3.Day3;
+// import day3.Day3;
+import day3.Day3Part2;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -18,7 +19,10 @@ public class App {
         // int result = new Day2Part2().processFile();
         // System.out.println(result);
 
-        int result = new Day3().processFile();
+        // int result = new Day3().processFile();
+        // System.out.println(result);
+
+        int result = new Day3Part2().processFile();
         System.out.println(result);
     }
 }
---
+++ src/day3/Day3Part2.java
@@ -0,0 +1,48 @@
+package day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day3Part2 {
+    public int processFile() {
+        try {
+            File file = new File("resources/day3.txt");
+            Scanner scanner = new Scanner(file);
+
+            String regex = "mul\\((\\d+),(\\d+)\\)";
+            Pattern pattern = Pattern.compile(regex);
+            
+            int sum = 0;
+
+            while (scanner.hasNextLine()) {
+                System.out.println("here");
+                String line = scanner.nextLine();
+                
+                String patternRemoval = "(?<=don't\\(\\)).*?(?=do\\(\\))";
+                String modifiedLine = line.replaceAll(patternRemoval, "");
+                Matcher matcher = pattern.matcher(modifiedLine);
+
+                while (matcher.find()) {
+                    String fullMatch = matcher.group(0);
+                    
+                    String firstInteger = matcher.group(1);
+                    String secondInteger = matcher.group(2);
+                    System.out.printf("%s %s %n",firstInteger,secondInteger);
+                    
+                    sum += Integer.parseInt(firstInteger) * Integer.parseInt(secondInteger);
+                    
+                    System.out.printf("Pattern: %s -> Integers: %s, %s%n", fullMatch, firstInteger, secondInteger);
+                }
+            }
+            
+            scanner.close();
+            return sum;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            return -1;
+        }
+    }
+}
