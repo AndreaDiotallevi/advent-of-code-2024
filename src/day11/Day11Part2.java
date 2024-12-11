ef8d0ed1dcf31262ff633607bb1c48f81b26aa84
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-11 15:47:11 +0000

    progress

--- src/day11/Day11Part2.java
+++ src/day11/Day11Part2.java
@@ -10,13 +10,12 @@ import java.util.Scanner;
 
 public class Day11Part2 {
     public List<Long> myList = new ArrayList<>();
-    public Long sum = (long)0;
-    public int times = 55;
+    public int times = 25;
     public Map<Integer,Long> mySet = new HashMap<>();
 
     public long processFile() {
         try {
-            File file = new File("resources/day11test2.txt");
+            File file = new File("resources/day11test.txt");
             Scanner scanner = new Scanner(file);
             String line = scanner.nextLine();
             String[] stringArray = line.split(" ");
@@ -24,9 +23,13 @@ public class Day11Part2 {
                 myList.add(Long.parseLong(s));
             }
             scanner.close();
+
+            Long sum = (long)0;
             for (int i=0; i<myList.size(); i++) {
                 System.out.println(i);
-                recursiveFind(myList.get(i), 0);
+                long partial = recursiveFind(myList.get(i), 0,0);
+                System.out.println(partial);
+                sum+= partial;
             }
             return sum; 
         } catch (FileNotFoundException e) {
@@ -50,55 +53,56 @@ public class Day11Part2 {
     // 1 x 45 = 125001266
     // 1 x 46 = 189148778
     // 1 x 50 = 1010392024
+    // 1 x 55 = 8161193535
 
-    private void recursiveFind(long num, int depth) {
+    private long recursiveFind(long num, int depth, long sum) {
         // System.out.printf("depth=%d, num=%d %n",depth,num);
         
-        if (num==1 && times-depth==3) {
-            sum+=4;
-            return;
-        }
-        if (num==1 && times-depth==10) {
-            sum+=62;
-            return;
-        }
-        if (num==1 && times-depth==20) {
-            sum+=3572;
-            return;
-        }
-        if (num==1 && times-depth==30) {
-            sum+=234511;
-            return;
-        }
-        if (num==1 && times-depth==40) {
-            sum+=15458147;
-            return;
-        }
-        if (num==1 && times-depth==46) {
-            sum+=189148778;
-            return;
-        }
-        if (num==1 && times-depth==50) {
-            sum+=1010392024;
-            return;
-        }
+        // if (num==1 && times-depth==3) {
+        //     sum+=4;
+        //     return;
+        // }
+        // if (num==1 && times-depth==10) {
+        //     sum+=62;
+        //     return;
+        // }
+        // if (num==1 && times-depth==20) {
+        //     sum+=3572;
+        //     return;
+        // }
+        // if (num==1 && times-depth==30) {
+        //     sum+=234511;
+        //     return;
+        // }
+        // if (num==1 && times-depth==40) {
+        //     sum+=15458147;
+        //     return;
+        // }
+        // if (num==1 && times-depth==46) {
+        //     sum+=189148778;
+        //     return;
+        // }
+        // if (num==1 && times-depth==50) {
+        //     sum+=1010392024;
+        //     return;
+        // }
 
         if (depth==times) {
             sum++;
             // System.out.println(sum);
-            return;
+            return 1;
         }
 
         if (num==0) {
-            recursiveFind(1, depth+1);
+            return recursiveFind(1, depth+1, sum);
         } else {
             String str = String.valueOf(num);
             int size = str.length();
             if (size%2==0) {
-                recursiveFind(Long.parseLong(str.substring(0, size/2)), depth+1);
-                recursiveFind(Long.parseLong(str.substring(size/2)), depth+1);
+                return (recursiveFind(Long.parseLong(str.substring(0, size/2)), depth+1,sum) +
+                        recursiveFind(Long.parseLong(str.substring(size/2)), depth+1,sum) );
             } else {
-                recursiveFind(num*2024, depth+1);
+                return recursiveFind(num*2024, depth+1,sum);
             }
         }
     }
