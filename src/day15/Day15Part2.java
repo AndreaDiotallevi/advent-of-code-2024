724ae0b98f3bda4aeb3676774594a9bac2c10b93
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-15 15:32:40 +0000

    progress'

--- src/day15/Day15Part2.java
+++ src/day15/Day15Part2.java
@@ -4,7 +4,9 @@ import java.awt.Point;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Day15Part2 {
@@ -14,7 +16,7 @@ public class Day15Part2 {
 
     public long processFile() {
         try {
-            File file = new File("resources/day15test.txt");
+            File file = new File("resources/day15test2.txt");
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -52,24 +54,52 @@ public class Day15Part2 {
                 }
             }
             scanner.close();
-            System.out.println(warehouse);
+            for (int y = 0; y < warehouse.size(); y++) {
+                for (int x = 0; x < warehouse.get(0).size(); x++) {
+                    System.out.print(warehouse.get(y).get(x));
+                }
+                System.out.println();
+            }
             System.out.println(robot);
 
-            // for (char movement : movements) {
-            // attemptToMove(robot, movement);
-            // }
-            // System.out.println(warehouse);
-            // System.out.println(robot);
+            for (char movement : movements) {
+                Map<Point, Character> swapMap = new HashMap<>();
+                swapMap.put(robot, '.');
+
+                Point vector = getVector(movement);
+                if (vector.x == 0) {
+                    System.out.println("vertically");
+                    boolean canMove = checkVertically(robot, movement, swapMap);
+                    System.out.println(swapMap);
+                    if (canMove) {
+                        System.out.println("can move vertically");
+                        moveCells(swapMap);
+                        robot.translate(vector.x, vector.y);
+                    } else {
+                        System.out.println("cannot move vertically");
+                    }
+                } else {
+                    System.out.println("horizontally");
+                    checkHorizontally(robot, movement);
+                }
+
+                for (int y = 0; y < warehouse.size(); y++) {
+                    for (int x = 0; x < warehouse.get(0).size(); x++) {
+                        System.out.print(warehouse.get(y).get(x));
+                    }
+                    System.out.println();
+                }
+            }
 
             long sum = 0;
 
-            // for (int x = 0; x < warehouse.get(0).size(); x++) {
-            // for (int y = 0; y < warehouse.size(); y++) {
-            // if (warehouse.get(y).get(x) == 'O') {
-            // sum += 100 * y + x;
-            // }
-            // }
-            // }
+            for (int x = 0; x < warehouse.get(0).size(); x++) {
+                for (int y = 0; y < warehouse.size(); y++) {
+                    if (warehouse.get(y).get(x) == 'O') {
+                        sum += 100 * y + x;
+                    }
+                }
+            }
 
             return sum;
         } catch (FileNotFoundException e) {
@@ -90,7 +120,48 @@ public class Day15Part2 {
             throw new Error("Movement char is invalid");
     }
 
-    private void attemptToMove(Point robot, char movement) {
+    private boolean checkVertically(Point point, char movement, Map<Point, Character> swapMap) {
+        swapMap.put(point, '.');
+        System.out.println();
+        char currentChar = warehouse.get(point.y).get(point.x);
+        System.out.println("current char=" + currentChar);
+        Point vector = getVector(movement);
+        Point nextPoint = new Point(point.x + vector.x, point.y + vector.y);
+        char nextChar = warehouse.get(nextPoint.y).get(nextPoint.x);
+        System.out.println("next char=" + nextChar);
+        if (nextChar == '#') {
+            return false;
+        }
+        if (nextChar == '[') {
+            boolean response1 = checkVertically(nextPoint, movement, swapMap);
+            Point nextPointRight = new Point(nextPoint.x + 1, nextPoint.y);
+            boolean response2 = checkVertically(nextPointRight, movement, swapMap);
+            swapMap.put(nextPoint, currentChar);
+            if (currentChar == '@') {
+                Point robotRight = new Point(point.x + 1, point.y + vector.y);
+                swapMap.put(robotRight, '.');
+            }
+            return response1 && response2;
+        }
+        if (nextChar == ']') {
+            boolean response1 = checkVertically(nextPoint, movement, swapMap);
+            Point nextPointLeft = new Point(nextPoint.x - 1, nextPoint.y);
+            boolean response2 = checkVertically(nextPointLeft, movement, swapMap);
+            swapMap.put(nextPoint, currentChar);
+            if (currentChar == '@') {
+                Point robotLeft = new Point(point.x - 1, point.y + vector.y);
+                swapMap.put(robotLeft, '.');
+            }
+            return response1 && response2;
+        }
+        if (nextChar == '.') {
+            swapMap.put(nextPoint, currentChar);
+            return true;
+        }
+        return true;
+    }
+
+    private void checkHorizontally(Point point, char movement) {
         Point vector = getVector(movement);
         Point firstEmptyCell = null;
 
@@ -127,7 +198,8 @@ public class Day15Part2 {
         boolean exit2 = false;
 
         while (!exit2) {
-            Point swapPointInner = new Point(swapPointOuter.x - vector.x, swapPointOuter.y - vector.y);
+            Point swapPointInner = new Point(swapPointOuter.x - vector.x,
+                    swapPointOuter.y - vector.y);
             // System.out.println("inner");
             // System.out.println(swapPointInner);
             char innerChar = warehouse.get(swapPointInner.y).get(swapPointInner.x);
@@ -143,6 +215,14 @@ public class Day15Part2 {
                 swapPointInner.translate(-vector.x, -vector.y);
                 swapPointOuter.translate(-vector.x, -vector.y);
             }
+        }
+    }
+
+    private void moveCells(Map<Point, Character> myMap) {
+        for (Map.Entry<Point, Character> entry : myMap.entrySet()) {
+            Point point = entry.getKey();
+            Character c = entry.getValue();
+            warehouse.get(point.y).set(point.x, c);
         }
     }
 }
