e7386eab5f5772622edd481083692f6507e1e0d6
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-15 12:55:58 +0000

    complete day 15 part 1

--- src/day15/Day15Part1.java
+++ src/day15/Day15Part1.java
@@ -14,7 +14,7 @@ public class Day15Part1 {
 
     public long processFile() {
         try {
-            File file = new File("resources/day15test.txt");
+            File file = new File("resources/day15.txt");
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -39,21 +39,32 @@ public class Day15Part1 {
             }
             scanner.close();
             System.out.println(warehouse);
-            System.out.println(movements);
             System.out.println(robot);
 
             for (char movement : movements) {
-
+                attemptToMove(robot, movement);
+                // break;
             }
+            System.out.println(warehouse);
+            System.out.println(robot);
 
             long sum = 0;
+
+            for (int x = 0; x < warehouse.get(0).size(); x++) {
+                for (int y = 0; y < warehouse.size(); y++) {
+                    if (warehouse.get(y).get(x) == 'O') {
+                        sum += 100 * y + x;
+                    }
+                }
+            }
+
             return sum;
         } catch (FileNotFoundException e) {
             return -1;
         }
     }
 
-    private Point getMovementPoint(char c) {
+    private Point getVector(char c) {
         if (c == '<')
             return new Point(-1, 0);
         if (c == '^')
@@ -64,5 +75,61 @@ public class Day15Part1 {
             return new Point(0, 1);
         else
             throw new Error("Movement char is invalid");
+    }
+
+    private void attemptToMove(Point robot, char movement) {
+        Point vector = getVector(movement);
+        Point firstEmptyCell = null;
+
+        Point nextPoint = new Point(robot.x + vector.x, robot.y + vector.y);
+        // System.out.println("next point");
+        // System.out.println(nextPoint);
+
+        boolean exit1 = false;
+        while (!exit1) {
+            char nextChar = warehouse.get(nextPoint.y).get(nextPoint.x);
+            if (nextChar == '.') {
+                // System.out.println(" found empty cell");
+                firstEmptyCell = nextPoint;
+                exit1 = true;
+                break;
+            }
+            if (nextChar == '#') {
+                // System.out.println("found border");
+                exit1 = true;
+                break;
+            }
+            nextPoint.translate(vector.x, vector.y);
+        }
+
+        if (firstEmptyCell == null) {
+            // System.out.println("no empty cell");
+            return;
+        }
+        // System.out.println("after");
+
+        Point swapPointOuter = new Point(firstEmptyCell.x, firstEmptyCell.y);
+        // System.out.println("outer");
+        // System.out.println(swapPointOuter);
+        boolean exit2 = false;
+
+        while (!exit2) {
+            Point swapPointInner = new Point(swapPointOuter.x - vector.x, swapPointOuter.y - vector.y);
+            // System.out.println("inner");
+            // System.out.println(swapPointInner);
+            char innerChar = warehouse.get(swapPointInner.y).get(swapPointInner.x);
+            // System.out.println("inner char");
+            // System.out.println(innerChar);
+            warehouse.get(swapPointOuter.y).set(swapPointOuter.x, innerChar);
+            warehouse.get(swapPointInner.y).set(swapPointInner.x, '.');
+            if (innerChar == '@') {
+                // System.out.println("robot found - stop");
+                robot.translate(vector.x, vector.y);
+                exit2 = true;
+            } else {
+                swapPointInner.translate(-vector.x, -vector.y);
+                swapPointOuter.translate(-vector.x, -vector.y);
+            }
+        }
     }
 }
