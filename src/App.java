a4854f32615ad3a365f797a11904255fd003e698
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-21 14:07:45 +0100

    complete day 18 part 1

--- src/App.java
+++ src/App.java
@@ -1,11 +1,10 @@
 import day18.Day18Part1;
-import day18.Day18Part1b;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // final long startTime = System.currentTimeMillis();
 
-        Day18Part1b.run();
+        Day18Part1.run();
 
         // System.out.println(result);
         // final long endTime = System.currentTimeMillis();
--- src/day18/Day18Part1.java
+++ src/day18/Day18Part1.java
@@ -1,41 +1,43 @@
 package day18;
 
+import java.awt.Point;
 import java.io.*;
 import java.util.*;
 
 public class Day18Part1 {
-    public static class Node {
-        int x, y, time;
+    public static List<List<Integer>> memory = new ArrayList<>();
+    public static List<List<Integer>> fallingBytes = new ArrayList<>();
+    public static int memorySize = 71;
+    public static int bytesCount = 1024;
+    public static int startX = 0;
+    public static int startY = 0;
+    public static int endX = memorySize - 1;
+    public static int endY = memorySize - 1;
+    public static List<Point> directions = new ArrayList<>(Arrays.asList(
+            new Point(0, 1),
+            new Point(1, 0),
+            new Point(0, -1),
+            new Point(-1, 0)));
 
-        public Node(int x, int y, int time) {
+    public static class State implements Comparable<State> {
+        int x, y, steps;
+        List<State> parents;
+
+        public State(int x, int y, int steps) {
             this.x = x;
             this.y = y;
-            this.time = time;
+            this.steps = steps;
         }
 
         @Override
-        public int hashCode() {
-            return Objects.hash(x, y, time);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            Node other = (Node) obj;
-            return x == other.x && y == other.y && time == other.time;
+        public int compareTo(State other) {
+            return Integer.compare(this.steps, other.steps);
         }
     }
 
-    public static Set<Node> memory = new HashSet<>();
-    public static List<List<Integer>> fallingBytes = new ArrayList<>();
-    public static int memorySize = 7;
-
     public static void readInput() {
         try {
-            File file = new File("resources/day18test.txt");
+            File file = new File("resources/day18.txt");
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
@@ -60,49 +62,78 @@ public class Day18Part1 {
 
     public static void createMemory() {
         for (int x = 0; x < memorySize; x++) {
+            List<Integer> row = new ArrayList<>();
             for (int y = 0; y < memorySize; y++) {
-                Node node = new Node(x, y, 0);
-                memory.add(node);
+                row.add(-1);
             }
+            memory.add(row);
         }
 
-        for (int time = 0; time < fallingBytes.size(); time++) {
+        for (int time = 0; time < bytesCount; time++) {
             List<Integer> fallingByte = fallingBytes.get(time);
-            Node node = new Node(fallingByte.get(0), fallingByte.get(1), time);
-            memory.add(node);
+            int row = fallingByte.get(0);
+            int col = fallingByte.get(1);
+            memory.get(row).set(col, time);
         }
     }
-
-    // public static void createMemory() {
-    // for (int x = 0; x < memorySize; x++) {
-    // List<Character> row = new ArrayList<>();
-    // for (int y = 0; y < memorySize; y++) {
-    // row.add('.');
-    // }
-    // memory.add(row);
-    // }
-
-    // for (List<Integer> fallingByte : fallingBytes) {
-    // int row = fallingByte.get(0);
-    // int col = fallingByte.get(1);
-    // memory.get(row).set(col, '#');
-    // }
-    // }
 
     public static void printMemory() {
         for (int x = 0; x < memorySize; x++) {
             for (int y = 0; y < memorySize; y++) {
-                System.out.print(memory.get(y).get(x));
+                String paddedNumber = String.format("%3d ", memory.get(y).get(x));
+                System.out.print(paddedNumber);
             }
             System.out.println();
         }
         System.out.println();
     }
 
+    public static boolean isArrived(int x, int y) {
+        return x == endX && y == endY;
+    }
+
+    public static boolean isValid(int x, int y) {
+        return x >= 0 && y >= 0 && x <= endX && y <= endY && memory.get(x).get(y) == -1;
+    }
+
+    public static long findShortestPath() {
+        PriorityQueue<State> queue = new PriorityQueue<>();
+        queue.add(new State(startX, startY, 0));
+        Set<String> visited = new HashSet<>();
+
+        while (!queue.isEmpty()) {
+            State current = queue.poll();
+            int x = current.x;
+            int y = current.y;
+            int steps = current.steps;
+
+            if (x == endX && y == endY) {
+                return steps;
+            }
+
+            String stateKey = x + "," + y;
+
+            if (visited.contains(stateKey)) {
+                continue;
+            }
+            visited.add(stateKey);
+
+            for (Point direction : directions) {
+                Point next = new Point(x + direction.x, y + direction.y);
+                if (isValid(next.x, next.y)) {
+                    queue.add(new State(next.x, next.y, steps + 1));
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void run() {
         readInput();
-        printFallingBytes();
+        // printFallingBytes();
         createMemory();
-        printMemory();
+        // printMemory();
+        long result = findShortestPath();
+        System.out.println(result);
     }
 }
