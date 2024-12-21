fa9fd3865b04e5bbd21661c2f4d6c2f285277c0e
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-21 12:14:44 +0100

    add numbers with time

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 import day18.Day18Part1;
+import day18.Day18Part1b;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // final long startTime = System.currentTimeMillis();
 
-        Day18Part1.run();
+        Day18Part1b.run();
 
         // System.out.println(result);
         // final long endTime = System.currentTimeMillis();
--- src/day18/Day18Part1b.java
+++ src/day18/Day18Part1b.java
@@ -3,33 +3,8 @@ package day18;
 import java.io.*;
 import java.util.*;
 
-public class Day18Part1 {
-    public static class Node {
-        int x, y, time;
-
-        public Node(int x, int y, int time) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y, time);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null || getClass() != obj.getClass())
-                return false;
-            Node other = (Node) obj;
-            return x == other.x && y == other.y && time == other.time;
-        }
-    }
-
-    public static Set<Node> memory = new HashSet<>();
+public class Day18Part1b {
+    public static List<List<Integer>> memory = new ArrayList<>();
     public static List<List<Integer>> fallingBytes = new ArrayList<>();
     public static int memorySize = 7;
 
@@ -60,39 +35,26 @@ public class Day18Part1 {
 
     public static void createMemory() {
         for (int x = 0; x < memorySize; x++) {
+            List<Integer> row = new ArrayList<>();
             for (int y = 0; y < memorySize; y++) {
-                Node node = new Node(x, y, 0);
-                memory.add(node);
+                row.add(-1);
             }
+            memory.add(row);
         }
 
         for (int time = 0; time < fallingBytes.size(); time++) {
             List<Integer> fallingByte = fallingBytes.get(time);
-            Node node = new Node(fallingByte.get(0), fallingByte.get(1), time);
-            memory.add(node);
+            int row = fallingByte.get(0);
+            int col = fallingByte.get(1);
+            memory.get(row).set(col, time);
         }
     }
-
-    // public static void createMemory() {
-    // for (int x = 0; x < memorySize; x++) {
-    // List<Character> row = new ArrayList<>();
-    // for (int y = 0; y < memorySize; y++) {
-    // row.add('.');
-    // }
-    // memory.add(row);
-    // }
-
-    // for (List<Integer> fallingByte : fallingBytes) {
-    // int row = fallingByte.get(0);
-    // int col = fallingByte.get(1);
-    // memory.get(row).set(col, '#');
-    // }
-    // }
 
     public static void printMemory() {
         for (int x = 0; x < memorySize; x++) {
             for (int y = 0; y < memorySize; y++) {
-                System.out.print(memory.get(y).get(x));
+                String paddedNumber = String.format("%3d ", memory.get(y).get(x));
+                System.out.print(paddedNumber);
             }
             System.out.println();
         }
