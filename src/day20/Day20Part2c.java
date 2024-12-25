601cf44b9d6cafb51d6e5b0690652137148fd0cd
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-12-25 10:36:02 +0100

    complete day 20 part 2

--- src/day20/Day20Part2c.java
+++ src/day20/Day20Part2c.java
@@ -6,12 +6,15 @@ import java.util.*;
 
 public class Day20Part2c {
     public static int trackSize = 141;
+    // public static int trackSize = 15;
     public static char[][] track = new char[trackSize][trackSize];
     public static int startX;
     public static int startY;
     public static int endX;
     public static int endY;
+    public static int maxCheat = 20;
     public static int minPicosecondsToSave = 100;
+    // public static int minPicosecondsToSave = 72;
     public static List<Point> directions = new ArrayList<>(Arrays.asList(
             new Point(0, 1),
             new Point(1, 0),
@@ -37,7 +40,7 @@ public class Day20Part2c {
 
     public static void readInput() {
         try {
-            File file = new File("resources/day20test.txt");
+            File file = new File("resources/day20.txt");
             Scanner scanner = new Scanner(file);
             int x = 0;
             while (scanner.hasNextLine()) {
@@ -125,14 +128,39 @@ public class Day20Part2c {
         return path;
     }
 
+    public static int getDistance(Point p1, Point p2) {
+        return Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+    }
+
     public static void run() {
         readInput();
         printTrack();
         System.out.printf("Start at (%d,%d)%n", startX, startY);
         System.out.printf("End at (%d,%d)%n", endX, endY);
         List<Point> path = findShortestPath();
-        System.out.println(path.size());
+
         Collections.reverse(path);
-        System.out.println(path);
+
+        long count = 0;
+
+        Map<Point, Integer> originalDistances = new HashMap<>();
+
+        for (int i = 0; i < path.size(); i++) {
+            Point point = path.get(i);
+            originalDistances.put(point, path.size() - i - 1);
+        }
+
+        for (Point start : path) {
+            for (Point end : path) {
+                int originalDistance = originalDistances.get(start);
+                int d1 = getDistance(start, end);
+                int d2 = originalDistances.get(end);
+                if (d1 <= maxCheat && originalDistance - (d1 + d2) >= minPicosecondsToSave) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
     }
 }
