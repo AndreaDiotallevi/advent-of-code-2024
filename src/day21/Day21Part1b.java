31fe596fd585a5d948fabd838a20195b9544ec6c
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2025-01-07 17:27:25 +0200

    add keys

--- src/day21/Day21Part1b.java
+++ src/day21/Day21Part1b.java
@@ -29,6 +29,23 @@ public class Day21Part1b {
         myMap.put(">-A", new ArrayList<>(List.of('^')));
         myMap.put(">-^", new ArrayList<>(List.of('<', '^')));
         myMap.put(">-v", new ArrayList<>(List.of('<')));
+        myMap.put("A-0", new ArrayList<>(List.of('<')));
+        myMap.put("0-2", new ArrayList<>(List.of('^')));
+        myMap.put("2-9", new ArrayList<>(List.of('^', '^', '>')));
+        myMap.put("9-A", new ArrayList<>(List.of('v', 'v', 'v')));
+        myMap.put("A-9", new ArrayList<>(List.of('^', '^', '^')));
+        myMap.put("9-8", new ArrayList<>(List.of('<')));
+        myMap.put("8-0", new ArrayList<>(List.of('v', 'v', 'v')));
+        myMap.put("0-A", new ArrayList<>(List.of('>')));
+        myMap.put("A-1", new ArrayList<>(List.of('^', '<', '<')));
+        myMap.put("1-7", new ArrayList<>(List.of('^', '^')));
+        myMap.put("7-9", new ArrayList<>(List.of('>', '>')));
+        myMap.put("A-4", new ArrayList<>(List.of('^', '^', '<', '<')));
+        myMap.put("4-5", new ArrayList<>(List.of('>')));
+        myMap.put("5-6", new ArrayList<>(List.of('>')));
+        myMap.put("6-A", new ArrayList<>(List.of('v', 'v')));
+        myMap.put("A-3", new ArrayList<>(List.of('^')));
+        myMap.put("3-7", new ArrayList<>(List.of('<', '<', '^', '^')));
     }
 
     public static List<Character> wordResult(String s) {
@@ -41,7 +58,7 @@ public class Day21Part1b {
 
         int depth = 0;
 
-        while (depth <= 1) {
+        while (depth <= 2) {
             char current = 'A';
             List<Character> result = new ArrayList<>();
             for (char c : charList) {
@@ -60,23 +77,43 @@ public class Day21Part1b {
         return charList;
     }
 
-    public static void run() {
-        List<String> words = new ArrayList<>(
-                Arrays.asList("<A^A>^^AvvvA", "^^^A<AvvvA>A", "^<<A^^A>>AvvvA", "^A<<^^A>>AvvvA", "^^<<A>A>AvvA"));
+    public static long findNumericPart(String s) {
+        String numericPart = s.replaceAll("[^0-9]", "");
+        int number = Integer.parseInt(numericPart);
+        return number;
+    }
 
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(29, 980, 179, 456, 379));
+    public static void run() {
+        // List<String> words = new ArrayList<>(
+        // Arrays.asList("<A^A>^^AvvvA", "^^^A<AvvvA>A", "^<<A^^A>>AvvvA",
+        // "^A<<^^A>>AvvvA", "^^<<A>A>AvvA"));
+
+        // List<Integer> numbers = new ArrayList<>(Arrays.asList(29, 980, 179, 456,
+        // 379));
+
+        // List<String> words = new ArrayList<>(
+        // Arrays.asList("^<<A>A^^>AvvvA", "^<<A^^A>>vAvvA", "^^^A<AvAvv>A",
+        // "^<<A^^A>vvvA>A", "^^<AvA^^A>vvvA"));
+
+        // List<Integer> numbers = new ArrayList<>(Arrays.asList(129, 176, 985, 170,
+        // 528));
+
+        List<String> words = new ArrayList<>(Arrays.asList("029A", "980A", "179A",
+                "456A", "379A"));
 
         long sum = 0;
 
         for (int i = 0; i < words.size(); i++) {
             String word = words.get(i);
-            int number = numbers.get(i);
             List<Character> result = wordResult(word);
             System.out.println(result);
             System.out.println(word + " = " + result.size());
-            sum += result.size() * number;
+            sum += result.size() * findNumericPart(word);
         }
 
         System.out.println(sum);
     }
 }
+
+// 141708 too high
+// 145896
